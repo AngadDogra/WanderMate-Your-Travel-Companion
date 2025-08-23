3451f4504a94280b200c9dacc1e3ec74405d58e1
AngadDogra <angaddogra1204@gmail.com>
2025-08-23 21:42:19 +0530

    Added Login and Signup stuff

--- plannerproject/globe/urls.py
+++ plannerproject/globe/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    # Auth routes
+    path('login/', views.CustomLoginView.as_view(), name='login'),
+    path('logout/', views.CustomLogoutView.as_view(), name='logout'),
+    path('signup/', views.signup, name='signup'),
 ]
 
